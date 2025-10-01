a30c37be1098fec65b6677c41cef26c7bb652caa
Ryo <alonsovelarde12@gmail.com>
2025-10-01 14:08:01 +0200

    Actividad 1

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
+import utilidades.ProcesadorTexto;
+import utilidades.Usuario;
+
 public class DatosUsuario {
         public static void main(String[] args) {
-        String 
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tu usuario:");
+        String nombreUsuario = sc.nextLine();
+        System.out.println("Tu edad:");
+        int edad = sc.nextInt();
+        System.out.println("Tu email:");
+        String correo = sc.nextLine();
+        String gmail = sc.nextLine();
+        boolean emailValido = ProcesadorTexto.esEmailValido(gmail);
+        if (emailValido){
+	System.out.println(" Tu email es valido ");
+} else {
+	System.out.println(" Tu email no es valido ");
+}
+       sc.close();
         }
 }
-
