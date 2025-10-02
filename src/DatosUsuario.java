92aeb08eb5420aeed192662e9f7df7aefb4d00fe
Ryo <alonsovelarde12@gmail.com>
2025-10-02 10:12:34 +0200

    EDADES

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -5,20 +5,22 @@ import utilidades.Usuario;
 
 public class DatosUsuario {
         public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Tu usuario:");
-        String nombreUsuario = sc.nextLine();
-        System.out.println("Tu edad:");
-        int edad = sc.nextInt();
-        System.out.println("Tu email:");
-        String correo = sc.nextLine();
-        String gmail = sc.nextLine();
-        boolean emailValido = ProcesadorTexto.esEmailValido(gmail);
-        if (emailValido){
-	System.out.println(" Tu email es valido ");
-} else {
-	System.out.println(" Tu email no es valido ");
-}
-       sc.close();
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Tu usuario:");
+                String nombreUsuario = sc.nextLine();
+                System.out.println("Tu edad:");
+                int edad = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Tu email:");
+
+                String correo = sc.nextLine();
+                boolean emailValido = ProcesadorTexto.esEmailValido(correo);
+                if (emailValido) {
+
+                System.out.println("El usuario si puede ser creado porque es valido");
+                } else {
+                System.out.println("El usuario no puede ser creado porque no es valido");
+                }
+
         }
 }
