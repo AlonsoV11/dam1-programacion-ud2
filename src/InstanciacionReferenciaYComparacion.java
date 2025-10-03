2692b2a12fc576596d6f9ae5416fa4c78b5d6569
Ryo <alonsovelarde12@gmail.com>
2025-10-03 14:14:39 +0200

    nstanciacionReferenciaYComparacion

--- src/InstanciacionReferenciaYComparacion.java
+++ src/InstanciacionReferenciaYComparacion.java
@@ -1,1 +1,42 @@
+import utilidades.Cancion;
 
+public class InstanciacionReferenciaYComparacion {
+
+    public static void main(String[] args) {
+        Cancion c1 = new Cancion();
+        Cancion c2 = new Cancion("Clocks", "Coldplay");
+        System.out.println(c2);
+
+        Cancion c3 = new Cancion("Fleeting Dream", "PARIS THE PRince");
+        Cancion c4 = new Cancion();
+        System.out.println(c3 == c4);
+        System.out.println(c3);
+
+        Cancion c5 = new Cancion("Hola", "c");
+        Cancion c6 = new Cancion();
+        System.out.println(c5.equals(c6));
+        System.out.println(c5);
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+        
+
+    }
+
+}
